9e70d4ebcda05cd78235c69334e1a40279a78c12
alvinlal <alvinzzz2001@gmail.com>
2022-04-09 15:32:18 +0530

    fix axios post bug 🐛

--- client/src/pages/user/UserDash.js
+++ client/src/pages/user/UserDash.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import '../../components/cards/cards.css';
-import { useHistory ,generatePath,useParams} from "react-router-dom"; 
+import { useHistory, generatePath, useParams } from 'react-router-dom';
 import CardItem from '../../components/carditem/CardItem.js';
 import axios from 'axios';
 import PostView from '../../pages/user/PostView';
@@ -30,131 +30,104 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
 const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
+  return <Slide direction='up' ref={ref} {...props} />;
 });
-
-
 
 const Alert = React.forwardRef(function Alert(props, ref) {
-  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />;
 });
 
-
-
 export default function UserDash() {
- 
-const[opens,setOpens]=React.useState(false);
+  const [opens, setOpens] = React.useState(false);
   const [open, setOpen] = React.useState(false);
-const[image,setImage]=useState('')
-const[title,setTitle]=useState('')
-const[desc,setDesc]=useState('')
-var[likes,setLikes]=useState('')
-var [postId, setId] = useState('');
-const[cat,setCat]=useState('');
-const [post,setPost]=useState('');
+  const [image, setImage] = useState('');
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
+  var [likes, setLikes] = useState('');
+  var [postId, setId] = useState('');
+  const [cat, setCat] = useState('');
+  const [post, setPost] = useState('');
 
+  // const [error, setError] = useState(null);
+  // const [isLoaded, setIsLoaded] = useState(false);
+  // const [items, setItems] = useState([]);
+  // const [q, setQ] = useState("");
+  // const [searchParam] = useState(["capital", "name", "numericCode"]);
+  // const [filterParam, setFilterParam] = useState(["All"]);
 
+  const [category, setCategory] = useState('');
+  // const handleChange = async(event) =>{
 
-// const [error, setError] = useState(null);
-// const [isLoaded, setIsLoaded] = useState(false);
-// const [items, setItems] = useState([]);
-// const [q, setQ] = useState("");
-// const [searchParam] = useState(["capital", "name", "numericCode"]);
-// const [filterParam, setFilterParam] = useState(["All"]); 
+  //   console.log('Value',event.target.value);
+  //   setCategory(event.target.value);
+  //   console.log('HandleChange',category)
 
+  // let res = await axios.post(process.env.REACT_APP_SERVER_URL + "/filterPost/getFilterPosts", {
+  //   headers: { Accept: 'application/json', "Content-Type": "application/json", },
+  //   credentials: 'include',
+  //   body:{'category':event.target.value}
+  // })
+  //  console.log('RES',res);
+  //   {content.map(renderCard)}
 
+  // };
 
+  const handleClickOpen = async id => {
+    setId(id);
 
-
-const [category, setCategory] = useState('');
-// const handleChange = async(event) =>{
-
-//   console.log('Value',event.target.value);
-//   setCategory(event.target.value);
-//   console.log('HandleChange',category)
-
-// let res = await axios.post(process.env.REACT_APP_SERVER_URL + "/filterPost/getFilterPosts", {
-//   headers: { Accept: 'application/json', "Content-Type": "application/json", },
-//   credentials: 'include',
-//   body:{'category':event.target.value}
-// })
-//  console.log('RES',res);
-//   {content.map(renderCard)}
-
-// };
-
-  const handleClickOpen = async(id) => {
-
-setId(id);
-
-    let res = await axios.post(process.env.REACT_APP_SERVER_URL + "/eachPost/getEachPosts", {
-      headers: { Accept: 'application/json', "Content-Type": "application/json", },
+    let res = await axios.post(process.env.REACT_APP_SERVER_URL + '/eachPost/getEachPosts', {
+      headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
       credentials: 'include',
-      body:{'content_Id':id}
-    })
-    setDesc(res.data[0].content)
-    setImage(res.data[0].content_Image)
+      body: { content_Id: id },
+    });
+    setDesc(res.data[0].content);
+    setImage(res.data[0].content_Image);
     // setTitle(res.data[0].content_Category)
-setTitle(res.data[0].content_Title)
-setLikes(res.data[0].content_Likes)
-setCat(res.data[0].cat_Name);
-    setPost(res.data)
-    
-    
-   
+    setTitle(res.data[0].content_Title);
+    setLikes(res.data[0].content_Likes);
+    setCat(res.data[0].cat_Name);
+    setPost(res.data);
 
-
-    console.log('res in eachPost',res.data)
-    console.log('EachPost',posts)
+    console.log('res in eachPost', res.data);
+    console.log('EachPost', posts);
     // post.map(function(cValue,idx){
     //   console.log("currentValue.id:",cValue.content_Title);
     // })
 
     // console.log('Data', post.data[0].content)
-  setOpen(true);
-  console.log('Dialog id', id)
-};
+    setOpen(true);
+    console.log('Dialog id', id);
+  };
 
-const [alert, setAlert] = useState(false);
-const handleClose = () => {
-  setOpen(false);
-};
+  const [alert, setAlert] = useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-  const handleLike=async()=>{
-    console.log('Post id',postId)
+  const handleLike = async () => {
+    console.log('Post id', postId);
     // console.log('Like id',id)
-    let res = await axios.post(process.env.REACT_APP_SERVER_URL + "/like/likePost", {
-      // mode: 'no-cors',
-            method: "POST",
-      headers: { Accept: 'application/json', "Content-Type": "application/json", },
-      credentials: 'include',
-      body:{'content_Id':postId}
-    });
-    console.log('Res',res)
-    if(res.data=='Already Liked')
-    {
+    let res = await axios.post(process.env.REACT_APP_SERVER_URL + '/like/likePost', { content_Id: postId }, { withCredentials: true });
+    console.log('Res', res);
+    if (res.data == 'Already Liked') {
       // console.log('user',user);
       // var datetime = new Date();
       // console.log("Date",datetime)
       setAlert(true);
-      setOpens(true)
-      
+      setOpens(true);
+
       // window.alert('Already liked')
       // <Alert severity="info">This is an info alert — check it out!</Alert>
-      
     }
-    if(res.data=='Liked')
-    {
-    setLikes(likes+1)
+    if (res.data == 'Liked') {
+      setLikes(likes + 1);
     }
-  }
-
-
+  };
 
   var posts;
   const [content, setContent] = useState([]);
   const fetchPosts = async () => {
-    let res = await fetch(process.env.REACT_APP_SERVER_URL + "/fetchPost/getApprovedPosts", {
+    let res = await fetch(process.env.REACT_APP_SERVER_URL + '/fetchPost/getApprovedPosts', {
       // mode: 'no-cors',
       credentials: 'include',
     });
@@ -162,166 +135,137 @@ const handleClose = () => {
     // (result) => {
     //   setIsLoaded(true);
     //   setItems(result);
-    console.log("this is res in fetchPosts", res);
+    console.log('this is res in fetchPosts', res);
     res = res.map(({ content_Id: id, ...rest }) => ({ id, ...rest }));
-
 
     posts = res;
 
-    console.log('posts in fetchPosts', posts)
-    
+    console.log('posts in fetchPosts', posts);
+
     //     for(var i=0;i<6;i++)
     //     {
     //  getTitle(posts[i].content)
     //     }
 
     // res.forEach((item, i) => { item.id = i + 1; });
-    // console.log('res', res) 
-    
-    setContent(res)
+    // console.log('res', res)
+
+    setContent(res);
     // console.log(res)
     // console.log('content',posts.cat_Name)
   };
-  
+
   useEffect(() => {
     fetchPosts();
-
   }, []);
 
+  var printId = id => {
+    console.log('This is id ', id);
+  };
 
-  var printId=(id)=>{
-    console.log("This is id ",id)
-  }
+  // const[post,setPost]=useState([])
+  const handleClick = async id => {
+    console.log('Handle Clicked', id);
 
-// const[post,setPost]=useState([])
-  const handleClick=
-  async (id) => {
-    console.log('Handle Clicked',id)
-    
-    let res = await axios.post(process.env.REACT_APP_SERVER_URL + "/eachPost/getEachPosts", {
-      headers: { Accept: 'application/json', "Content-Type": "application/json", },
+    let res = await axios.post(process.env.REACT_APP_SERVER_URL + '/eachPost/getEachPosts', {
+      headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
       credentials: 'include',
-      body:{'content_Id':id}
+      body: { content_Id: id },
     });
-    
-    console.log('Handle Clicked',id)
-    
+
+    console.log('Handle Clicked', id);
+
     // res = await res.json();
 
-    
-    console.log("this is res",res.data);
+    console.log('this is res', res.data);
 
     // res = res.map(({ content_Id: id, ...rest }) => ({ id, ...rest }));
     // res.forEach((item, i) => { item.id = i + 1; });
-    // console.log('res', res) 
-    
-    setPost(res.data)
-    console.log('EachPost',post)
+    // console.log('res', res)
 
-};
+    setPost(res.data);
+    console.log('EachPost', post);
+  };
 
-const handleClickSnack = () => {
-  setOpens(true);
-};
+  const handleClickSnack = () => {
+    setOpens(true);
+  };
 
-const handleCloseSnack = (event, reason) => {
-  if (reason === 'clickaway') {
-    return;
-  }
+  const handleCloseSnack = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
 
-  setOpens(false);
-};
-
-
+    setOpens(false);
+  };
 
   const renderCard = (card, index) => {
     // console.log("POSTS",content[0].cat_Name);
-    console.log("Category", category)
-   
+    console.log('Category', category);
+
     // let dataBuffer = new Buffer(card.content_Image);
     // let mediaType = 'PNG';
     // console.log('category',category);
     // console.log('category',cat);
     return (
-
-<>
-
-
-      <div className='cards' onClick={() => {handleClickOpen(card.id)}}>
-
-        <CardItem
-      
-          src={card.content_Image}
-          id={card.id}
-            text={card.content_Title}
-          label={card.cat_Name}
-          path='/UserDash'
-          
-         
-        />
-      {/* <ThumbDownIcon/>
+      <>
+        <div
+          className='cards'
+          onClick={() => {
+            handleClickOpen(card.id);
+          }}
+        >
+          <CardItem src={card.content_Image} id={card.id} text={card.content_Title} label={card.cat_Name} path='/UserDash' />
+          {/* <ThumbDownIcon/>
       <ThumbUpIcon/> */}
-      
-     </div>
+        </div>
 
-
-<div >
-      
-  
-      <Dialog
-        
-        fullScreen
-        open={open}
-        onClose={handleClose}
-        TransitionComponent={Transition}
-      >
-       
-      
-               <AppBar sx={{ position: 'relative' }}>
-          <Toolbar>
-            <IconButton
-              edge="start"
-              color="inherit"
-              onClick={handleClose}
-              aria-label="close"
-            >
-              <CloseIcon />
-            </IconButton>
-            <Typography sx={{ ml: 170, marginTop:1 }} variant="h6" component="div">
-              {likes} <ThumbUpIcon />
-              <Button  autoFocus color="inherit" onClick={() => {handleLike(card.id)}}>
-              Like
-            </Button>
-            </Typography>
-            {/* <Button autoFocus color="inherit" onClick={handleAccept}>
+        <div>
+          <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
+            <AppBar sx={{ position: 'relative' }}>
+              <Toolbar>
+                <IconButton edge='start' color='inherit' onClick={handleClose} aria-label='close'>
+                  <CloseIcon />
+                </IconButton>
+                <Typography sx={{ ml: 170, marginTop: 1 }} variant='h6' component='div'>
+                  {likes} <ThumbUpIcon />
+                  <Button
+                    autoFocus
+                    color='inherit'
+                    onClick={() => {
+                      handleLike(card.id);
+                    }}
+                  >
+                    Like
+                  </Button>
+                </Typography>
+                {/* <Button autoFocus color="inherit" onClick={handleAccept}>
               Accept
             </Button>
             <Button autoFocus color="inherit" onClick={handleReject}>
               Reject
             </Button> */}
-          </Toolbar>
-        </AppBar>
+              </Toolbar>
+            </AppBar>
 
-        <h1>{title}</h1>
-        <img src={image} style={{width:'80%', height:'50%', margin:'1px 106px'}}></img>
-        
-       <p style={{margin: '53px 113px', display:'block',fontSize: 15,lineHeight:'2'}}>
-         {desc}</p>
-         <Snackbar open={opens} autoHideDuration={6000} onClose={handleCloseSnack}>
-         {alert?  <Alert onClose={handleCloseSnack} severity="info">You have already liked the post!</Alert>: <></> }
-         </Snackbar>
-       </Dialog>
-    </div>
+            <h1>{title}</h1>
+            <img src={image} style={{ width: '80%', height: '50%', margin: '1px 106px' }}></img>
 
-    </>
-      
-
-
+            <p style={{ margin: '53px 113px', display: 'block', fontSize: 15, lineHeight: '2' }}>{desc}</p>
+            <Snackbar open={opens} autoHideDuration={6000} onClose={handleCloseSnack}>
+              {alert ? (
+                <Alert onClose={handleCloseSnack} severity='info'>
+                  You have already liked the post!
+                </Alert>
+              ) : (
+                <></>
+              )}
+            </Snackbar>
+          </Dialog>
+        </div>
+      </>
     );
-          
-
   };
-
 
   // <Card style={{ width: "18rem" }} key={index} className="box">
   //   {/* <Card.Img variant="top" src="holder.js/100px180" src={card.image} /> */}
@@ -331,24 +275,22 @@ const handleCloseSnack = (event, reason) => {
   //   </Card.Body>
   // </Card>
 
+  //   const [posts, setposts] = useState([]);
 
-//   const [posts, setposts] = useState([]);
+  //   const [selectedCategory, setSelectedCategory] = useState();
 
-//   const [selectedCategory, setSelectedCategory] = useState();
-  
-//   function getFilteredList() {
-//     if (!selectedCategory) {
-//       return posts;
-//     }
-//     return posts.filter((item) => item.category === selectedCategory);
-//   }
+  //   function getFilteredList() {
+  //     if (!selectedCategory) {
+  //       return posts;
+  //     }
+  //     return posts.filter((item) => item.category === selectedCategory);
+  //   }
 
+  //   var filteredList = useMemo(getFilteredList, [selectedCategory, posts]);
 
-//   var filteredList = useMemo(getFilteredList, [selectedCategory, posts]);
-
-
-  return <>
-  {/* <div style={{ margin: '40px 200px 5px ' ,width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+  return (
+    <>
+      {/* <div style={{ margin: '40px 200px 5px ' ,width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
   <Box  sx={{maxWidth: 150,display: 'inline', gap: 15 ,width: 120}}>
     <FormControl fullWidth>
       <InputLabel id="demo-simple-select-label">Category</InputLabel>
@@ -366,13 +308,13 @@ const handleCloseSnack = (event, reason) => {
     </FormControl>
   </Box>
   </div> */}
-  <div className="grid" 
-  style={{ width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-     {content.map(renderCard)}
-    {/* {filteredList.content.map(renderCard)} */}
-    </div>;
+      <div className='grid' style={{ width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        {content.map(renderCard)}
+        {/* {filteredList.content.map(renderCard)} */}
+      </div>
+      ;
     </>
-
+  );
 
   // var[title,getTitle]=useState([])
 
@@ -381,8 +323,6 @@ const handleCloseSnack = (event, reason) => {
   //   getTitle(posts[i].content);
   //   console.log('title',title)
   // }
-
-
 
   // return (
   //   <div className="grid">{posts.map(renderCard)}</div>);
@@ -431,8 +371,6 @@ const handleCloseSnack = (event, reason) => {
   //   </div>
 
   // );
-};
-  
+}
+
 // };
-
-
